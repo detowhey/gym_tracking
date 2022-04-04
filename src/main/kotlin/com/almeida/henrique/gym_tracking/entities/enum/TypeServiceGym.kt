af24032a94a0b7371ba58eaabedddaa8bcec7184
Henrique de Freitas Almeida <56054329+detowhey@users.noreply.github.com>
2022-04-04 01:55:42 -0300

    changing enums

--- src/main/kotlin/com/almeida/henrique/gym_tracking/entities/enum/TypeServiceGym.kt
+++ src/main/kotlin/com/almeida/henrique/gym_tracking/entities/enum/TypeServiceGym.kt
@@ -1,13 +1,18 @@
 package com.almeida.henrique.gym_tracking.entities.enum
 
-enum class TypeServiceGym(val code: Int) {
+enum class TypeServiceGym(val code: Int, name: String) {
 
-    BODYBUIILDING(1), COMBAT_SPORTS(2), FITNESS(3), POWERLIFT(4);
+    BODYBUIILDING(1, "Bobybuilding"),
+    COMBAT_SPORTS(2, "Combat sports"),
+    FITNESS(3, "Fitness"),
+    POWERLIFTTING(4, "Powerlifting");
 
 
     companion object {
         fun valueOf(code: Int): TypeServiceGym {
             return values().find { it.code == code } ?: throw IllegalArgumentException("Invalide Type Service Gym Code")
         }
+
+        fun getName(code: Int): String = valueOf(code).name
     }
 }
